ca730d26aa01b2a6862309bd63b4250844b20718
przemek <przemek.bialkwno@gmail.com>
2026-03-02 18:21:05 +0100

    adding default photo, basic style for product card component

--- app/_components/ui/ProductCard.tsx
+++ app/_components/ui/ProductCard.tsx
@@ -1,19 +1,36 @@
 import { Item } from "@models/Item";
+import Image from "next/image";
+import { Button } from "./button";
 
 interface ItemsProps {
   items: Item[];
 }
 
 const ProductCard = ({ items }: ItemsProps) => {
+  const handleAdd = () => {
+    console.log("added");
+  };
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-2 justify-center">
       {items.map((item) => (
-        <div key={item._id}>
+        <div
+          key={item._id}
+          className="bg-slate-200 p-4 rounded-lg flex flex-col items-center gap-2"
+        >
+          <div className="relative ">
+            <Image
+              src={item.image ?? "/placeholder.png"}
+              width={240}
+              height={240}
+              alt={item.name}
+              className="object-cover"
+            />
+          </div>
           <h4>{item.name}</h4>
           <p>on stock: {item.qtn}</p>
           <p>price: {item.price} pln</p>
+          <Button onClick={handleAdd}>add</Button>
           <p>{item.description}</p>
-          <img src={item.image} />
         </div>
       ))}
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,16 @@
 import HomeCarousel from "@components/home/HomeCarousel";
 import Products from "@components/ui/Products";
-import dbConnect from "@lib/mongodb";
-import Item from "@models/Item";
+// import dbConnect from "@lib/mongodb";
+// import Item from "@models/Item";
 
 const Home = async () => {
-  await dbConnect();
-  const items = await Item.find({});
-  console.log(items);
+  // await dbConnect();
+  // const items = await Item.find({});
+  // console.log(items);
 
   return (
     <div>
       <HomeCarousel />
-      <div>
-        {items.map((item) => (
-          <p key={item._id}>{item.name}</p>
-        ))}
-      </div>
-
-      <p>test</p>
       <Products />
     </div>
   );
