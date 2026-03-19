e5abeafb7e1277e5d7d08291f4a288fb49eecd83
przemek <przemek.bialkwno@gmail.com>
2026-03-19 16:45:33 +0100

    protecting the route for admins

--- app/api/items/[id]/route.ts
+++ app/api/items/[id]/route.ts
@@ -1,11 +1,25 @@
+import { auth } from "@clerk/nextjs/server";
 import dbConnect from "@lib/mongodb";
 import Item from "@models/Item";
+import { authorizeAdmin } from "app/_services/user.service";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(
   request: NextRequest,
   { params }: { params: { id: string } },
 ) {
+  // protecting route
+  const { userId } = await auth();
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const admin = await authorizeAdmin(userId);
+
+  if (!admin) {
+    return new Response("Forbidden", { status: 403 });
+  }
+
   try {
     const { id } = params;
     const {
